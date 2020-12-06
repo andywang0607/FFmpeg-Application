43e13b2930c8771dcce6d034ece5c38bb285e113
YiChih Wang <andywang0607@gmail.com>
2020-12-06 23:11:30 +0800

    [chore] Add VideoCapture factory
    Note:
    - Implement a concrete VideoCapture with opencv

---
+++ MultiMedia2RTMP/VideoCaptureFactory.cpp
@@ -0,0 +1,75 @@
+#include "VideoCaptureFactory.h"
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+#pragma comment(lib, "opencv_worldd.lib")
+
+using namespace std;
+using namespace cv;
+
+class CVideoCapture :public VideoCaptureFactory
+{
+public:
+    VideoCapture cam;
+
+    void run()
+    {
+        Mat frame;
+        while (!isExit)
+        {
+            if (!cam.read(frame))
+            {
+                msleep(1);
+                continue;
+            }
+            if (frame.empty())
+            {
+                msleep(1);
+                continue;
+            }
+            Data d((char*)frame.data, frame.cols*frame.rows*frame.elemSize());
+            Push(d);
+        }
+    }
+
+    bool Init(int camIndex = 0)
+    {
+        cam.open(camIndex);
+        if (!cam.isOpened())
+        {
+            cout << "cam open failed!" << endl;
+            return false;
+        }
+        cout << camIndex << " cam open success" << endl;
+        cam.set(CV_CAP_PROP_FPS, 30);
+        width = cam.get(CAP_PROP_FRAME_WIDTH);
+        height = cam.get(CAP_PROP_FRAME_HEIGHT);
+        fps = cam.get(CAP_PROP_FPS);
+        if (fps == 0) fps = 30;
+        return true;
+    }
+
+    void Stop()
+    {
+        DataManager::Stop();
+        if (cam.isOpened())
+        {
+            cam.release();
+        }
+    }
+};
+
+VideoCaptureFactory::VideoCaptureFactory()
+{
+}
+
+
+VideoCaptureFactory::~VideoCaptureFactory()
+{
+}
+
+VideoCaptureFactory *VideoCaptureFactory::Get(unsigned char index)
+{
+    static CVideoCapture instances[255];
+    return &instances[index];
+}
---
+++ MultiMedia2RTMP/VideoCaptureFactory.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "datamanager.h"
+
+class VideoCaptureFactory : public DataManager
+{
+public:
+    static VideoCaptureFactory *Get(unsigned char index = 0);
+    virtual bool Init(int camIndex = 0) = 0;
+    virtual void Stop() = 0;
+
+    virtual ~VideoCaptureFactory();
+
+    int width = 0;
+    int height = 0;
+    int fps = 0;
+protected:
+    VideoCaptureFactory();   
+};
+
